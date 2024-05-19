add156ec1043e127bee5f2d93936edad7d2bd70c
Michael Javier <m.mota@concentra.com.do>
2024-05-19 12:46:13 -0400

    pb

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button, Container } from "@mui/material";
 export default function App() {
   return (
     // <Container sx={{ border: "2px", borderStyle: "solid" }}>
-    <Container sx={{ border: 2, boxShadow: 3 }}>
+    <Container sx={{ border: 2, boxShadow: 3, pb: 3 }}>
       <Button variant="contained"> Mi primer boton</Button>
     </Container>
   );
