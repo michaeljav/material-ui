b22f37dcf5beeeda32b04fbb99ca80efe29ec9cc
Michael Javier <m.mota@concentra.com.do>
2024-05-19 13:41:00 -0400

    icono

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { Box, Button, Container, Typography } from "@mui/material";
-
+import AirplanemodeInactiveIcon from "@mui/icons-material/AirplanemodeInactive";
 export default function App() {
   return (
     <Container sx={{ border: 2, boxShadow: 3, pb: 3 }}>
       <Typography color="primary">HOAL</Typography>
-      <Button variant="contained"> Mi primer boton</Button>
+      <Button variant="contained" color="error">
+        {" "}
+        error
+      </Button>
+      <Button
+        variant="contained"
+        color="success"
+        startIcon={<AirplanemodeInactiveIcon />}
+      >
+        {" "}
+        success
+      </Button>
       <Box sx={{ p: 5 }}>BOX</Box>
       <Box
         sx={{
