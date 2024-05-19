5d65a5bcba13d329e11a2a38c7f22e22608e4c14
Michael Javier <m.mota@concentra.com.do>
2024-05-19 12:45:26 -0400

    working  material ui

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button, Container } from "@mui/material";
+
+export default function App() {
+  return (
+    // <Container sx={{ border: "2px", borderStyle: "solid" }}>
+    <Container sx={{ border: 2, boxShadow: 3 }}>
+      <Button variant="contained"> Mi primer boton</Button>
+    </Container>
+  );
+}
