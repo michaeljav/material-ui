9c61ffe30dc9824efb11f3bdb523d7a7756ee272
Michael Javier <m.mota@concentra.com.do>
2024-05-19 13:01:44 -0400

    typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Button, Container } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 
 export default function App() {
   return (
-    // <Container sx={{ border: "2px", borderStyle: "solid" }}>
     <Container sx={{ border: 2, boxShadow: 3, pb: 3 }}>
+      <Typography variant="h2" component={"h2"} textAlign={"center"}>
+        typography
+      </Typography>
       <Button variant="contained"> Mi primer boton</Button>
     </Container>
   );
